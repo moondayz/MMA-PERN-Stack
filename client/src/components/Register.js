74966831b102f56d0d3c29aeb4a88a7849227b86
Elif Aygun <elifdilaa@gmail.com>
2020-12-24 17:06:03 +0300

    Removed unused libraries

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Link, Redirect } from "react-router-dom";
+import { Link} from "react-router-dom";
 import { toast } from "react-toastify";
 
 // firstName, lastName, phoneNumber, email, passwordUser, maritalStatus, gender, placeOfBirth, dateOfBirth
